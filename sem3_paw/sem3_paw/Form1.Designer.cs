a65fa01e23ed6c3c845f5527a3f27f1c7c2ef0c0
Cristian Ciurea <cristianciurea@gmail.com>
2020-03-04 12:43:04 +0200

    update

---
+++ sem3_paw/sem3_paw/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace sem3_paw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbCod = new System.Windows.Forms.TextBox();
+            this.tbVarsta = new System.Windows.Forms.TextBox();
+            this.tbNume = new System.Windows.Forms.TextBox();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cod";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sex";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Varsta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(167, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nume";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(167, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Note";
+            // 
+            // tbCod
+            // 
+            this.tbCod.Location = new System.Drawing.Point(241, 41);
+            this.tbCod.Name = "tbCod";
+            this.tbCod.Size = new System.Drawing.Size(100, 20);
+            this.tbCod.TabIndex = 5;
+            // 
+            // tbVarsta
+            // 
+            this.tbVarsta.Location = new System.Drawing.Point(241, 130);
+            this.tbVarsta.Name = "tbVarsta";
+            this.tbVarsta.Size = new System.Drawing.Size(100, 20);
+            this.tbVarsta.TabIndex = 6;
+            // 
+            // tbNume
+            // 
+            this.tbNume.Location = new System.Drawing.Point(241, 193);
+            this.tbNume.Name = "tbNume";
+            this.tbNume.Size = new System.Drawing.Size(100, 20);
+            this.tbNume.TabIndex = 7;
+            // 
+            // tbNote
+            // 
+            this.tbNote.Location = new System.Drawing.Point(241, 240);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(100, 20);
+            this.tbNote.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.comboBox1.Location = new System.Drawing.Point(241, 81);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Adauga";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 333);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.tbNume);
+            this.Controls.Add(this.tbVarsta);
+            this.Controls.Add(this.tbCod);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbCod;
+        private System.Windows.Forms.TextBox tbVarsta;
+        private System.Windows.Forms.TextBox tbNume;
+        private System.Windows.Forms.TextBox tbNote;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ sem3_paw/sem3_paw/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sem3_paw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Student s2 = new Student(123, 'M', 23, "Gigel",
+                new int[3] { 6, 8, 9 });
+            Student s3 = (Student)s2.Clone();
+            Console.WriteLine(s1);
+            s2 = s2 + 10;
+            s2 += 5;
+            s2++;
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            s2.SpuneAnNastere();
+            Console.WriteLine("Media lui s2 este: " + (float)s2);
+            Console.WriteLine("A 2-a nota a lui s2: " + s2[1]);
+            s2[5] = 5;
+            Console.WriteLine(s2);
+            Console.WriteLine("Media lui s2 este: " + s2.CalculeazaMedie());
+
+            List<Student> listaStud = new List<Student>();
+            listaStud.Add(s3);
+            listaStud.Add(s2);
+            listaStud.Add(s1);
+            listaStud.Sort();
+            foreach (Student s in listaStud)
+                Console.WriteLine(s);
+
+            Form1 frm = new Form1();
+            frm.ShowDialog();
+        }
+    }
+}
