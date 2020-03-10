3142a8620a7312436a19556f58ed4540e8f77b54
Cristian Ciurea <cristianciurea@gmail.com>
2020-03-10 20:41:14 +0200

    update

---
+++ sem4_paw/sem4_paw/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace sem4_paw
+{
+    public partial class Form2 : Form
+    {
+        List<Student> lista2;
+
+        public Form2(List<Student> lista)
+        {
+            InitializeComponent();
+            lista2 = lista;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            foreach (Student s in lista2)
+                textBox1.Text += s.ToString() + Environment.NewLine;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "(*.txt)|*.txt";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(dlg.FileName);
+                sw.WriteLine(textBox1.Text);
+                sw.Close();
+                textBox1.Clear();
+            }
+        }
+    }
+}
