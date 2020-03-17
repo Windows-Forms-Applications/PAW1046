fcc80ea3c541858a29b290272f103508bbbd3777
Cristian Ciurea <cristianciurea@gmail.com>
2020-03-17 18:34:35 +0200

    update

---
+++ sem5_paw/sem4_paw/Form3.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace sem4_paw
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("nbrfxrates.xml");
+            string str = sr.ReadToEnd();
+            sr.Close();
+
+            XmlReader reader = XmlReader.Create(new StringReader(str));
+            while (reader.Read())
+            {
+                if (reader.Name == "PublishingDate" && reader.NodeType == XmlNodeType.Element)
+                {
+                    reader.Read();
+                    tbData.Text = reader.Value;
+                }
+                if (reader.Name == "Rate" && reader.NodeType == XmlNodeType.Element)
+                {
+                    string atribut = reader["currency"];
+                    if (atribut == "EUR")
+                    {
+                        reader.Read();
+                        tbEUR.Text = reader.Value;
+                    }
+                    else
+                        if (atribut == "USD")
+                        {
+                            reader.Read();
+                            tbUSD.Text = reader.Value;
+                        }
+                        else
+                            if (atribut == "GBP")
+                            {
+                                reader.Read();
+                                tbGBP.Text = reader.Value;
+                            }
+                            else
+                                if (atribut == "XAU")
+                                {
+                                    reader.Read();
+                                    tbXAU.Text = reader.Value;
+                                }
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MemoryStream memStream = new MemoryStream();
+            XmlTextWriter writer = new XmlTextWriter(memStream, Encoding.UTF8);
+            writer.Formatting = Formatting.Indented;
+
+            writer.WriteStartDocument();
+            writer.WriteStartElement("CursValutar");
+
+            writer.WriteStartElement("CursEUR");
+            writer.WriteValue(tbEUR.Text);
+            writer.WriteEndElement();
+
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+
+            writer.Close();
+
+            string str = Encoding.UTF8.GetString(memStream.ToArray());
+            memStream.Close();
+
+            StreamWriter sw = new StreamWriter("fisier.xml");
+            sw.WriteLine(str);
+            sw.Close();
+            MessageBox.Show("ok");
+        }
+    }
+}
